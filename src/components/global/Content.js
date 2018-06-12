dd8c7f13e6434aa9f270fba14fa0dcdbdf220c23
Leonardo Casales <leonardo@smartnet.com.ar>
2018-06-11 23:50:23 -0300

    bootstrap navBar added

---
+++ src/components/global/Content.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import logo from './media/logo.png';
+// import './css/Header.css';
+
+class Content extends Component {
+  constructor() {
+    super();
+
+    // this.handleClickLogo = this.handleClickLogo.bind(this);
+  }
+
+  // handleClickLogo(e) {
+  //   this.props.clickHideBar(e);
+  // }
+
+  render() {
+    // const {logoHideBar} = this.props;
+    return (
+        <div>Hola</div>
+
+    );
+    }
+}
+
+export default Content;
